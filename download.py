7794b092be3005f03ccfcd28d954469a972ef823
Laszlo Csontos <github@laszlocsontos.com>
2016-07-22 09:34:30 +0200

    Show completition

--- download.py
+++ download.py
@@ -9,6 +9,8 @@ def main():
     print "Usage: %s <master.json URL> <path to save>" % sys.argv[0]
     sys.exit(1)
   download_video(sys.argv[1], sys.argv[2])
+  print "Completed: %s" % sys.argv[1]
 
 if __name__ == "__main__":
     main()
+
